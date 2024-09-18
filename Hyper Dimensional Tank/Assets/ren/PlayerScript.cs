a515c5772b2ba558796cbf478965cf91a9f5dfe9
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2024-09-18 19:02:07 +0900

    2024/09/18_ren

--- Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
+++ Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private float rotateSpeed;
-    private float rotateAngle;
+    private float bodyRotateSpeed = 50f;
+    private float headRotateSpeed = 50f;
+    Vector3 moveSpeed = new Vector3(0, 0, 1f);
+    Transform head;
     // Start is called before the first frame update
     void Start()
     {
-        rotateSpeed = 0f;
-        rotateAngle = 0f;
+        head = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
             // Quaternion.AngleAxis(度数法, 軸);
-            rotateAngle++;
-           this.transform.rotation = Quaternion.AngleAxis(rotateAngle, Vector3.up);
+            this.transform.Translate(moveSpeed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Quaternion.AngleAxis(度数法, 軸);
+            this.transform.Translate(-moveSpeed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Quaternion.AngleAxis(度数法, 軸);
+           this.transform.rotation *= Quaternion.AngleAxis(bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Quaternion.AngleAxis(度数法, 軸);
+            this.transform.rotation *= Quaternion.AngleAxis(-bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Quaternion.AngleAxis(度数法, 軸);
+            head.rotation *= Quaternion.AngleAxis(headRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Quaternion.AngleAxis(度数法, 軸);
+            head.rotation *= Quaternion.AngleAxis(-headRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+
+
     }
 }
