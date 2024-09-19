83774308979a401bbf9f1000420b9af34073e27d
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2024-09-19 19:01:32 +0900

    20240919_ren2

--- Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
+++ Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //プレイヤーの動き
     private float bodyRotateSpeed = 50f;
     private float headRotateSpeed = 50f;
     Vector3 moveSpeed = new Vector3(0, 0, 1f);
     Transform head;
+
+    //弾の動き
+    //弾の発射場所
+    public GameObject shotPoint;
+
+    //弾
+    public GameObject bullet;
+
+    //弾の速さ
+    private float bulletSpeed = 100f;
+
+    //クールタイム
+    private float shotCoolTime = 0f;
+    private bool isShot = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +33,34 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMove();
+
        
+
+        if(isShot)
+        {
+            shotCoolTime = 0.0f;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //弾の発射する場所を取得する
+                Vector3 bulletPosition = shotPoint.transform.position;
+                //
+                GameObject newBullet = Instantiate(bullet, bulletPosition, head.gameObject.transform.rotation);
+                Vector3 dir = newBullet.transform.forward;
+                newBullet.GetComponent<Rigidbody>().AddForce(dir * bulletSpeed * Time.deltaTime, ForceMode.Impulse);
+                isShot = false;
+            }
+        }
+        else
+        {
+            if(shotCoolTime > 60.0f)
+            {
+                isShot = true;
+            }
+            shotCoolTime++;
+        }
+
+
     }
 
     private void PlayerMove()
