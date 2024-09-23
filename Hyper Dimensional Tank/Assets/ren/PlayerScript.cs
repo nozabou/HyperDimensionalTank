56ea1bf7615b215084af6dbae2eaec2bc08bb467
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2024-09-23 15:50:10 +0900

    20240923_ren

--- Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
+++ Hyper Dimensional Tank/Assets/ren/PlayerScript.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Transactions;
+using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerScript : MonoBehaviour
 {
     //プレイヤーの動き
-    float inputVertical;
-    float inputHorizontal;
+    private float moveSpeed = 5f;
+    private Vector3 moveVec;
+   
+    private Rigidbody rb;
+    //float inputVertical;
+    //float inputHorizontal;
 
     private float bodyRotateSpeed = 50f;
     private float headRotateSpeed = 50f;
@@ -26,7 +33,7 @@ public class PlayerScript : MonoBehaviour
     public GameObject bullet;
 
     //弾の速さ
-    private float bulletSpeed = 100f;
+    private float bulletSpeed = 200f;
 
     //クールタイム
     private int countCoolTime = 0;
@@ -39,10 +46,17 @@ public class PlayerScript : MonoBehaviour
     public int playerStock = 2;
 
 
+
+    //InputSystem
+   private PlayerControl playerControl;
+
     // Start is called before the first frame update
     void Start()
     {
+        moveVec = new Vector3(0,0,moveSpeed);
         head = transform.GetChild(0);
+        playerControl = new PlayerControl();
+        playerControl.Enable();
     }
 
     // Update is called once per frame
@@ -53,14 +67,21 @@ public class PlayerScript : MonoBehaviour
         {
             return;
         }
-        
         PlayerMove();
 
-       
-
-        if(isShot)
+        if (isShot)
         {
             countCoolTime = 0;
+            if (playerControl.Player.Fire.triggered)//押された瞬間
+            {
+                //弾の発射する場所を取得する
+                Vector3 bulletPosition = shotPoint.transform.position;
+                //
+                GameObject newBullet = Instantiate(bullet, bulletPosition, head.gameObject.transform.rotation);
+                Vector3 dir = newBullet.transform.forward;
+                newBullet.GetComponent<Rigidbody>().AddForce(dir * bulletSpeed * Time.deltaTime, ForceMode.Impulse);
+                isShot = false;
+            }
             //if (Input.GetKeyDown(KeyCode.Space))
             //{
             //    //弾の発射する場所を取得する
@@ -84,9 +105,80 @@ public class PlayerScript : MonoBehaviour
        
 
     }
+    //private void OnEnable()
+    //{
+    //    // 全体のActionを有効化
+    //    playerControl.Enable();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    // 全体のActionを無効化
+    //    playerControl.Disable();
+    //}
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        print("Fire Actionが呼ばれた！");
+    }
+    public void OnForward(InputAction.CallbackContext context)
+    {
+        this.transform.Translate(moveVec * Time.deltaTime);
+    }
+    public void OnBackward(InputAction.CallbackContext context)
+    {
+        this.transform.Translate(-moveVec * Time.deltaTime);
+    }
+    public void OnBodyRotationRight(InputAction.CallbackContext context)
+    {
+        // Quaternion.AngleAxis(度数法, 軸);
+        this.transform.rotation *= Quaternion.AngleAxis(bodyRotateSpeed * Time.deltaTime, Vector3.up);
+    }
+    public void OnBodyRotationLeft(InputAction.CallbackContext context)
+    {
+        // Quaternion.AngleAxis(度数法, 軸);
+        this.transform.rotation *= Quaternion.AngleAxis(-bodyRotateSpeed * Time.deltaTime, Vector3.up);
+    }
 
     private void PlayerMove()
     {
+
+        //前進と後退
+        //if (playerControl.Player.Forward.IsPressed())//押されている間
+        //{
+        //    this.transform.Translate(moveVec * Time.deltaTime);
+        //}
+
+        //if (playerControl.Player.Backward.IsPressed())//押されている間
+        //{
+        //    this.transform.Translate(-moveVec * Time.deltaTime);
+        //}
+        //回転
+        //if (playerControl.Player.BodyRotationRight.IsPressed())//押されている間
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    this.transform.rotation *= Quaternion.AngleAxis(bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
+        //if (playerControl.Player.BodyRotationLeft.IsPressed())//押されている間
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    this.transform.rotation *= Quaternion.AngleAxis(-bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
+
+        //大砲の向きの回転
+        //ActionMaps/Action/入力のふるまい
+        if (playerControl.Player.HeadRotationLeft.IsPressed())//押されている間
+        {
+            //Quaternion.AngleAxis(度数法, 軸);
+            head.rotation *= Quaternion.AngleAxis(-headRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+        if (playerControl.Player.HeadRotationRight.IsPressed())//押されている間
+        {
+            //Quaternion.AngleAxis(度数法, 軸);
+            head.rotation *= Quaternion.AngleAxis(headRotateSpeed * Time.deltaTime, Vector3.up);
+        }
+
+
+
         //inputVertical = Input.GetAxis("Vertical");
         //inputHorizontal = Input.GetAxis("Horizontal");
 
@@ -114,28 +206,28 @@ public class PlayerScript : MonoBehaviour
         //}
 
 
-            //if (Input.GetKey(KeyCode.D))
-            //{
-            //    // Quaternion.AngleAxis(度数法, 軸);
-            //    this.transform.rotation *= Quaternion.AngleAxis(bodyRotateSpeed * Time.deltaTime, Vector3.up);
-            //}
-            //if (Input.GetKey(KeyCode.A))
-            //{
-            //    // Quaternion.AngleAxis(度数法, 軸);
-            //    this.transform.rotation *= Quaternion.AngleAxis(-bodyRotateSpeed * Time.deltaTime, Vector3.up);
-            //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    this.transform.rotation *= Quaternion.AngleAxis(bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    this.transform.rotation *= Quaternion.AngleAxis(-bodyRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
 
 
-            //if (Input.GetKey(KeyCode.RightArrow))
-            //{
-            //    // Quaternion.AngleAxis(度数法, 軸);
-            //    head.rotation *= Quaternion.AngleAxis(headRotateSpeed * Time.deltaTime, Vector3.up);
-            //}
-            //if (Input.GetKey(KeyCode.LeftArrow))
-            //{
-            //    // Quaternion.AngleAxis(度数法, 軸);
-            //    head.rotation *= Quaternion.AngleAxis(-headRotateSpeed * Time.deltaTime, Vector3.up);
-            //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    head.rotation *= Quaternion.AngleAxis(headRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    // Quaternion.AngleAxis(度数法, 軸);
+        //    head.rotation *= Quaternion.AngleAxis(-headRotateSpeed * Time.deltaTime, Vector3.up);
+        //}
 
 
     }
