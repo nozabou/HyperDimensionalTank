2ef61e6759b28b7f72b1398030025fbbcb81d9fc
nozabou <166367304+nozabou@users.noreply.github.com>
2024-10-02 18:59:21 +0900

    up

--- Hyper Dimensional Tank/Assets/noza/ModoSelect/a.cs
+++ Hyper Dimensional Tank/Assets/noza/ModoSelect/a.cs
@@ -10,17 +10,84 @@ using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
     int cursorNum = 1;
-
+    GameObject cursor;
+    public float speed = 1.0f;
+    private float time;
+    private TextMeshProUGUI single;
+    private TextMeshProUGUI multi;
+    private TextMeshProUGUI title;
+    GameObject singleObj;
+    GameObject multiObj;
+    GameObject titleObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        singleObj = GameObject.Find("Single").gameObject;
+        multiObj = GameObject.Find("Multi").gameObject;
+        titleObj = GameObject.Find("Title").gameObject;
+        cursor = GameObject.Find("Cursor").gameObject;
+        single = singleObj.GetComponent<TextMeshProUGUI>();
+        multi = multiObj.GetComponent<TextMeshProUGUI>();
+        title =titleObj.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cursorNum++;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            cursorNum--;
+            
+        }
+        if (cursorNum == 1)
+        {
+            title.color = GetTextColorAlpha(title.color);
+            single.color = new Color32(0, 0, 0, 255);
+            multi.color = new Color32(0, 0, 0, 255);
+            cursor.transform.localPosition = new Vector3(-230, -144, 0);
+        }
+        if (cursorNum == 2)
+        {
+            multi.color = GetTextColorAlpha(multi.color);
+            title.color = new Color32(0, 0, 0, 255);
+            single.color = new Color32(0, 0, 0, 255);
+            cursor.transform.localPosition = new Vector3(-300, -38, 0);
+        }
+        if (cursorNum == 3)
+        {
+            single.color = GetTextColorAlpha(single.color);
+            multi.color = new Color32(0, 0, 0, 255);
+            title.color = new Color32(0, 0, 0, 255);
+            cursor.transform.localPosition = new Vector3(-363, 66, 0);
+        }
+        // スペースキーが押されたら決定
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cursorNum == 1)
+            {
+                Debug.Log("ひとりで");
+            }
+            if (cursorNum == 2)
+            {
+                Debug.Log("みんなで");
+            }
+            if (cursorNum == 2)
+            {
+                Debug.Log("タイトルに戻る");
+            }
+        }
+    }
+    Color GetTextColorAlpha(Color color)
+    {
+        time += Time.deltaTime * speed * 5.0f;
+        color.a = Mathf.Sin(time);
+
+        return color;
     }
 }
